88b4f30cbeb553fe47d5fd56da734ba297a49c3d
fly <fly@DESKTOP-66HQ90T>
2016-01-10 16:27:18 +0800

    -static file manage

--- Attendance_system/Attendance_system/settings.py
+++ Attendance_system/Attendance_system/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Users',
+    'base',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -123,4 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
--- Attendance_system/Attendance_system/urls.py
+++ Attendance_system/Attendance_system/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+
+from django.conf.urls.static import static  
+from django.conf import settings  
+
 from Users.views import login
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
