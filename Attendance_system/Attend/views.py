6100345e77ce23e77f58260cee99b0cb72fae148
fly <fly@DESKTOP-66HQ90T>
2016-01-20 16:33:10 +0800

    -menu re-implement

--- Attendance_system/Attend/views.py
+++ Attendance_system/Attend/views.py
@@ -4,7 +4,24 @@ from django.shortcuts import render
 
 from models import Attend
 
+from base.models import Menu
+
 def index(req):
     attends =  Attend.objects.filter(userId=1)
+    menu = __createMenu()
+    return render(req, "Attend/index.html",context={'menu':menu,'lines':attends})
+
+
+def __createMenu():
+    menu_1 = Menu('Attend')
+    menu_1.addMenu('All','http://www.baidu.com')
+    menu_1.addMenu('All','http://www.baidu.com')
     
-    return render(req, "Attend/index.html",context={'lines':attends})
+    
+    
+    menu_2 = Menu('Test1')
+    menu_2.addMenu('All','http://www.baidu.com')
+    menu_2.addMenu('All','http://www.baidu.com')
+    return (menu_1,menu_2)
+    
+    
--- Attendance_system/base/models.py
+++ Attendance_system/base/models.py
@@ -3,3 +3,23 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+# base class 
+# menu
+
+class Menu():
+    def __init__(self, topName):
+        self.topName = topName
+        
+        self.subMenus = []
+        
+    
+    def addMenu(self, title, url):
+        self.subMenus.append((title,url))
+        
+    def get_topName(self):
+        return self.topName
+    
+    def get_subMenus(self):
+        return self.subMenus
