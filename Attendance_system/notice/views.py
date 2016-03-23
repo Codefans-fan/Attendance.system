dc547b927fe470041785e89b200e82266a622613
fky <fky@DSASH-FKY>
2016-03-23 14:35:23 +0800

    +

--- Attendance_system/notice/views.py
+++ Attendance_system/notice/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def notice_index(req):
-    return render(req, 'notice_main.html')
+    return render(req, 'notice/notice_main.html')
