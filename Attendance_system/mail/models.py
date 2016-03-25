9476d383c3ade97ed7501622dcab5483c712d214
fky <fky@DSASH-FKY>
2016-03-25 18:25:20 +0800

    -mail

--- Attendance_system/mail/models.py
+++ Attendance_system/mail/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib import admin
 
+from django.contrib.auth.models import User
 # Create your models here.
+from django import forms
+
+
+class mailconfig(models.Model):
+    userid = models.ForeignKey(User,on_delete=models.CASCADE)
+    umail = models.EmailField()
+    mail_template = models.TextField()
+    
+    class Admin(admin.ModelAdmin):
+        list_display = ('userid', 'umail')
+
+
+
+class EmailEditForm(forms.Form):
+    username = forms.CharField(max_length=32,required=True,widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    umail = forms.EmailField(required=True)
+    mailtemplate= forms.CharField(widget=forms.Textarea)
