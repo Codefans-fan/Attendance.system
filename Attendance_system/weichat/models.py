47b869df8f6a78e04b149ea1b717d63d48c66bdf
fly <fly@DESKTOP-66HQ90T>
2016-03-26 10:44:34 +0800

    3

--- Attendance_system/weichat/models.py
+++ Attendance_system/weichat/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 from django.contrib import admin
 # Create your models here.
 
-from django.forms import ModelForm
 from django import forms
 
 
-from itertools import chain
+import wechat_utils
+
 class  user_weichat(models.Model):
     userid = models.ForeignKey(User,on_delete=models.CASCADE)
     weichatname = models.CharField(max_length = 64)
@@ -17,10 +17,20 @@ class  user_weichat(models.Model):
     
 
 
-class edit_form(ModelForm):
-    def __init__(self, uid, *args, **kwargs):
-        super(edit_form, self).__init__(*args, **kwargs)
+class WechatEditForm(forms.Form):
+    username = forms.CharField(max_length=32,required=True,widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    shortname = forms.CharField(max_length=16,required=True)
+    
+    def __init__(self, readonly_shortname=False, *args, **kwargs):
+        super(WechatEditForm, self).__init__(*args, **kwargs)
+        if readonly_shortname:
+            self.fields['shortname'].widget.attrs['readonly'] = True
+            
+    def clean(self):
+        cleaned_data = super(WechatEditForm, self).clean()
+        shortname = cleaned_data.get("shortname")
+        token = wechat_utils.get_weichat_token()
+        if not wechat_utils.validate_weichat_user(shortname,token):
+            #validate failed
+            self.add_error('shortname','wechat validate error. Please ensure you have follow DSA account.')                                                            
         
-    class Meta:
-        model = user_weichat
-        fields = ('userid', 'weichatname')  
