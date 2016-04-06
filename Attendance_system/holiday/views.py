8b90e0ebb3fc68797eeb6baea5aebb7934f23958
fky <562867448@qq.com>
2016-04-06 15:14:32 +0800

    -

--- Attendance_system/holiday/views.py
+++ Attendance_system/holiday/views.py
@@ -14,7 +14,12 @@ import datetime
 def syc_holiday_from_baidu(req):
     http = httplib2.Http()
     timestamp = str(time.time()).replace('.','')
-    data = {'query':datetime.datetime.now().strftime('%Y-%m'),'resource_id':'6018','ie':'utf8','oe':'gbk','cb':'op_aladdin_callback','co':'','format':'json','tn':'baidu','cb':'jQuery1102018497919710353017_'+str(timestamp),'t':str(timestamp),'_':str(timestamp)}
+    if req.GET.get('str_date',''):
+        query_date = req.GET.get('str_date','')
+    else:
+        query_date = datetime.datetime.now().strftime('%Y-%m')
+    print query_date
+    data = {'query':query_date,'resource_id':'6018','ie':'utf8','oe':'gbk','cb':'op_aladdin_callback','co':'','format':'json','tn':'baidu','cb':'jQuery1102018497919710353017_'+str(timestamp),'t':str(timestamp),'_':str(timestamp)}
     url = 'https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?'
     for key,val in data.items():
         url += key +'=' + val +'&'
@@ -27,6 +32,8 @@ def syc_holiday_from_baidu(req):
             for item in holiday_list:
                 if len(item['list']):
                     for day_holiday in item['list']:
+                        if day_holiday.get('status') != '1':
+                            continue
                         res = holiday_cn.objects.filter(holiday_name=item['name'],day=day_holiday['date'])
                         if not res:
                             holiday_cn(holiday_name =item['name'],day=day_holiday['date']).save()
